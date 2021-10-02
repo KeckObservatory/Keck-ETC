295c4dec4cdfec5fb658a2f71b565112ff133356
Keaton-WMKO <kblair@vm-internship.keck.hawaii.edu>
2021-10-01 16:18:19 -1000

    Added gitignore, vars()[] test in script

--- run_test.py
+++ run_test.py
@@ -2,23 +2,21 @@ from Instrument import instrument
 from Atmosphere import atmosphere
 from Source import source
 
-class run_test:
 
-    def __init__(self):
 
-        foo = instrument('test')
+foo = instrument('test')
 
-        print(foo)
-        print(foo.config.who)
-        print(foo.config.why)
-        print(foo.config.why.when)
-        print(foo.config.why.how)
+print(foo)
+print(foo.config.who)
+print(foo.config.why)
+print(vars(foo.config.why)['when'])
+print(foo.config.why.how)
 
-        bar = atmosphere()
+bar = atmosphere()
 
-        print(bar.config.defaults.seeing)
-        print(bar.seeing)
-        print(bar.airmass)
-        print(bar.water_vapor)
+print(bar.config.defaults.seeing)
+print(bar.seeing)
+print(bar.airmass)
+print(vars(bar)['water_vapor'])
 
-        yum = source()
+yum = source()
