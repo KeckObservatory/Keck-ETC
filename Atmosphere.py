1c8b5055f22d2a1674661f8b167fd67de9284b2c
Keaton-WMKO <kblair@keck.hawaii.edu>
2021-10-11 16:06:46 -1000

    Updated atmosphere headers, added interpolation (currently broken)

--- Atmosphere.py
+++ Atmosphere.py
@@ -1,6 +1,9 @@
 from astropy import units as u
 from astropy.table import Table
 import yaml
+from os import listdir
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
 
 class atmosphere:
 
@@ -28,10 +31,36 @@ class atmosphere:
 
 
     def _load_files(self):
-        # TODO
-        test = Table.read('sky_background/mk_skybg_zm_10_10_ph.dat', format='ascii.ecsv')
-        print(test)
-        print(test['wavelength'][1])
+        self.water_vapor_index = [u.Quantity(x) for x in self.config.water_vapor_index]
+        self.airmass_index = [u.Quantity(x) for x in self.config.airmass_index]
+        self.config.wavelength_index = [u.Quantity(x).to('angstrom') for x in self.config.wavelength_index]
+        self.wavelength_index = np.arange(
+            u.Quantity(self.config.wavelength_index[0]).value,
+            u.Quantity(self.config.wavelength_index[1]).value,
+            u.Quantity(self.config.wavelength_index[2]).value
+        ) * u.Quantity(self.config.wavelength_index[0]).unit  # np.arange doesn't support units, see https://github.com/astropy/astropy/issues/11582
+        self.transmission = np.zeros([len(self.airmass_index), len(self.water_vapor_index), len(self.wavelength_index)])
+        self.emission = np.zeros([len(self.airmass_index), len(self.water_vapor_index), len(self.wavelength_index)])
+        # Iterate through directory, filling in self.transmission and self.emission arrays
+        for i, filename in enumerate(listdir(self.config.file_directory)):
+            print('\rAtmosphere: reading file '+str(i+1)+'/'+str(len(listdir(self.config.file_directory))), end='')
+            if filename.startswith(self.config.transmission_filepath):
+                data = Table.read(self.config.file_directory+'/'+filename, format='ascii.ecsv')
+                self.transmission[
+                    [x == u.Quantity(data.meta['airmass']) for x in self.airmass_index], 
+                    [x == u.Quantity(data.meta['water_vapor']) for x in self.water_vapor_index], 
+                    :
+                ] = data['transmission']
+            if filename.startswith(self.config.emission_filepath):
+                data = Table.read(self.config.file_directory+'/'+filename, format='ascii.ecsv')
+                self.emission[
+                    [x == u.Quantity(data.meta['airmass']) for x in self.airmass_index], 
+                    [x == u.Quantity(data.meta['water_vapor']) for x in self.water_vapor_index], 
+                    :
+                ] = data['flux']
+        print()  # newline for console output
+
+
 
 
     def __init__(self):
@@ -48,11 +77,38 @@ class atmosphere:
         self._load_files()
 
 
+    def _trilinear_interpolation(self, wavelengths):
+        # TODO
+        return []
+
 
     def get_transmission(self, wavelengths):
-        # TODO
-        return []
+        interpolation = RegularGridInterpolator( (self.airmass_index, self.water_vapor_index, self.wavelength_index), self.transmission )
+        return interpolation([[self.airmass, self.water_vapor, λ] for λ in wavelengths])
 
-    def get_emmission(self, wavelengths):
-        # TODO
-        return []
+
+    def get_emission(self, wavelengths):
+        # Check for airmass, water vapor values on grid points
+        if self.airmass in self.airmass_index:
+            return []  # TODO
+        if self.water_vapor in self.water_vapor_index:
+            return []  # TODO
+
+        # Get airmass & vapor index values closest to current airmass & vapor
+        print(self.water_vapor_index)
+        print(self.water_vapor)
+        print(self.water_vapor_index < self.water_vapor)
+        am = [ np.array(self.airmass_index)[self.airmass_index < self.airmass][-1], np.array(self.airmass_index)[self.airmass_index > self.airmass][0] ]
+        wv = [ self.water_vapor_index[self.water_vapor_index < self.water_vapor][-1], self.water_vapor_index[self.water_vapor_index > self.water_vapor][0] ]
+        emissions = 1 / ( np.diff(am)*np.diff(wv) ) * \
+            [ [am[1]*wv[1], -am[1]*wv[0], -am[0]*wv[1], am[0]*wv[0]],
+            [-wv[1], wv[0], wv[1], -wv[0]],
+            [-am[1], am[1], am[0], -am[0]],
+            [1, -1, -1, 1] ] * \
+            [self.emissions[self.airmass_index==am[0], self.airmass_index==wv[0]],
+            self.emissions[self.airmass_index==am[0], self.airmass_index==wv[1]],
+            self.emissions[self.airmass_index==am[1], self.airmass_index==wv[0]],
+            self.emissions[self.airmass_index==am[1], self.airmass_index==wv[1]]]
+        emissions = [1, self.airmass, self.water_vapor, self.airmass*self.water_vapor] * emissions
+
+        return np.interp(wavelengths, self.wavelength_index, emissions)
--- run_test.py
+++ run_test.py
@@ -2,6 +2,7 @@ from Instrument import instrument
 from Atmosphere import atmosphere
 from Source import source
 import astropy.units as u
+import time
 
 
 if __name__ == '__main__':
@@ -10,7 +11,16 @@ if __name__ == '__main__':
     print(foo.get_dark_current())
     print(foo.get_read_noise())
 
+
     bar = atmosphere()
+    bar.airmass=u.Quantity(1.3)
+    bar.water_vapor=u.Quantity('4 mm')
+
+    start=time.time()
+    print(bar.get_emission(u.Quantity([0.3, 0.5, 0.8, 1, 1.5, 2, 2.5, 3], u.micron)))
+    print(bar.get_transmission(u.Quantity([0.3, 0.5, 0.8, 1, 1.5, 2, 2.5, 3], u.micron)))
+    end=time.time()
+    print('Time:', end-start)
 
     yum = source()
     yum.redshift = 2
