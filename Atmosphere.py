32da3a07ac1600941e20e89f2c576d9f8f3e7d02
Keaton-WMKO <kblair@keck.hawaii.edu>
2021-10-13 11:22:32 -1000

    Config validation for atmosphere_config.yaml

--- Atmosphere.py
+++ Atmosphere.py
@@ -9,7 +9,7 @@ from scipy.interpolate import RegularGridInterpolator
 
 class atmosphere:
 
-    global _config_filepath; _config_filepath = '/usr/local/home/kblair/Documents/ETC/prototype/sky_background/atmosphere_config.yaml'
+    global _CONFIG_FILEPATH; _CONFIG_FILEPATH = '/usr/local/home/kblair/Documents/ETC/prototype/sky_background/atmosphere_config.yaml'
 
     def _mount_config(self, config_path):
         # From https://www.geeksforgeeks.org/convert-nested-python-dictionary-to-object/
@@ -35,7 +35,7 @@ class atmosphere:
 
 
     def _load_files(self):
-        self.water_vapor_index = [u.Quantity(x).value for x in self.config.water_vapor_index] * u.Quantity(self.config.water_vapor_index[0]).unit
+        self.water_vapor_index = [u.Quantity(x).to(u.mm).value for x in self.config.water_vapor_index] * u.mm
         self.airmass_index = u.Quantity(self.config.airmass_index)
         self.config.wavelength_index = [u.Quantity(x).to(u.angstrom) for x in self.config.wavelength_index]
         self.wavelength_index = np.arange(
@@ -47,32 +47,83 @@ class atmosphere:
         self.emission = np.zeros([len(self.airmass_index), len(self.water_vapor_index), len(self.wavelength_index)])
         # Iterate through directory, filling in self.transmission and self.emission arrays
         for i, filename in enumerate(listdir(self.config.file_directory)):
-            print('\rAtmosphere: reading file '+str(i+1)+'/'+str(len(listdir(self.config.file_directory))), end='')
-            if filename.startswith(self.config.transmission_filepath):
-                data = Table.read(self.config.file_directory+'/'+filename, format='ascii.ecsv')
-                self.transmission[
-                    [x == u.Quantity(data.meta['airmass']) for x in self.airmass_index], 
-                    [x == u.Quantity(data.meta['water_vapor']) for x in self.water_vapor_index],
-                    :
-                ] = data['transmission'].to('')
-            if filename.startswith(self.config.emission_filepath):
-                data = Table.read(self.config.file_directory+'/'+filename, format='ascii.ecsv')
-                self.emission[
-                    [x == u.Quantity(data.meta['airmass']) for x in self.airmass_index], 
-                    [x == u.Quantity(data.meta['water_vapor']) for x in self.water_vapor_index], 
-                    :
-                ] = data['flux'].to('photon/(s arcsec^2 nm m^2)')
+            print('\rATMOSPHERE: Reading file '+str(i+1)+'/'+str(len(listdir(self.config.file_directory))), end='')
+            try:
+                if filename.startswith(self.config.transmission_filepath):
+                    data = Table.read(self.config.file_directory+'/'+filename, format='ascii.ecsv')
+                    self.transmission[
+                        [x == u.Quantity(data.meta['airmass']) for x in self.airmass_index], 
+                        [x == u.Quantity(data.meta['water_vapor']) for x in self.water_vapor_index],
+                        :
+                    ] = data['transmission'].to('')
+                if filename.startswith(self.config.emission_filepath):
+                    data = Table.read(self.config.file_directory+'/'+filename, format='ascii.ecsv')
+                    self.emission[
+                        [x == u.Quantity(data.meta['airmass']) for x in self.airmass_index], 
+                        [x == u.Quantity(data.meta['water_vapor']) for x in self.water_vapor_index], 
+                        :
+                    ] = data['flux'].to('photon/(s arcsec^2 nm m^2)')
+            except ValueError:
+                raise ValueError('ERROR: In atmosphere._load_files() -- invalid file contents')
         print()  # newline for console output
 
 
+    def _validate_config(self):
+        # Throw errors if config file doesn't conform to requirements
+        print('ATMOSPHERE: Validating configuration file',_CONFIG_FILEPATH)
+        
+        # Check defaults.water_vapor and water_vapor_index
+        test_wvi = [u.Quantity(x) for x in self.config.water_vapor_index]
+        if not test_wvi == sorted(test_wvi):
+            raise ValueError('ERROR: In atmosphere_config.yaml -- water_vapor_index is not in ascending order')
+        if not test_wvi[0] <= u.Quantity(self.config.defaults.water_vapor) <= test_wvi[-1]:
+            raise ValueError('ERROR: In atmosphere_config.yaml -- default water vapor is outside bounds of water_vapor_index')
+        
+        # Check defaults.airmass and airmass_index
+        test_ami = [u.Quantity(x) for x in self.config.airmass_index]
+        if not test_ami == sorted(test_ami):
+            raise ValueError('ERROR: In atmosphere_config.yaml -- airmass_index is not in ascending order')
+        if not test_ami[0] <= u.Quantity(self.config.defaults.airmass) <= test_ami[-1]:
+            raise ValueError('ERROR: In atmosphere_config.yaml -- default airmass is outside bounds of airmass_index')
+
+        # Check wavelength_index
+        test_wi = [u.Quantity(x) for x in self.config.wavelength_index]
+        if len(test_wi) != 3:
+            raise ValueError('ERROR: In atmosphere_config.yaml -- wavelength_index does not have length == 3')
+        if not (all([x > 0*u.nm for x in test_wi]) and test_wi[2] < (test_wi[1] - test_wi[0])):
+            raise ValueError('ERROR: In atmosphere_config.yaml -- wavelength_index does not match format (start, end, step)')
+
+        # Check emission_filepath and emission files
+        test_em = [x for x in listdir(self.config.file_directory) if x.startswith(self.config.emission_filepath)]
+        if not test_em:
+            raise ValueError('ERROR: In atmosphere_config.yaml -- file_directory or emission_filepath is invalid, no matching files')
+        try:
+            if not all([u.Quantity(Table.read(self.config.file_directory+'/'+x, format='ascii.ecsv', data_end=10).meta['water_vapor']) in test_wvi for x in test_em]):
+                raise ValueError('ERROR: In atmosphere_config.yaml -- File matching emission_filepath has water_vapor that does not match water_vapor_index')
+            if not all([u.Quantity(Table.read(self.config.file_directory+'/'+x, format='ascii.ecsv', data_end=10).meta['airmass']) in test_ami for x in test_em]):
+                raise ValueError('ERROR: In atmosphere_config.yaml -- File matching emission_filepath has airmass that does not match airmass_index')
+        except:
+            raise ValueError('ERROR: In atmosphere_config.yaml -- File matching emission_filepath does not have a valid ECSV file header, check metadata')
+
+        # Check transmission_filepath and transmission files
+        test_tr = [x for x in listdir(self.config.file_directory) if x.startswith(self.config.transmission_filepath)]
+        if not test_tr:
+            raise ValueError('ERROR: In atmosphere_config.yaml -- file_directory or transmission_filepath is invalid, no matching files')
+        try:
+            if not all([u.Quantity(Table.read(self.config.file_directory+'/'+x, format='ascii.ecsv', data_end=10).meta['water_vapor']) in test_wvi for x in test_tr]):
+                raise ValueError('ERROR: In atmosphere_config.yaml -- File matching transmission_filepath has water_vapor that does not match water_vapor_index')
+            if not all([u.Quantity(Table.read(self.config.file_directory+'/'+x, format='ascii.ecsv', data_end=10).meta['airmass']) in test_ami for x in test_tr]):
+                raise ValueError('ERROR: In atmosphere_config.yaml -- File matching transmission_filepath has airmass that does not match airmass_index')
+        except:
+            raise ValueError('ERROR: In atmosphere_config.yaml -- File matching transmission_filepath does not have a valid ECSV file header, check metadata')
+
+        
 
 
     def __init__(self):
-        # Set default values based on config file
-        self._mount_config(_config_filepath)
+        self._mount_config(_CONFIG_FILEPATH)
 
-        # TODO -- input validation here...
-        #   - Check consistent units in water_vapor?
+        self._validate_config()
 
         self.seeing = [ u.Quantity(self.config.defaults.seeing) ]
 
@@ -102,23 +153,25 @@ class atmosphere:
 
 
     def get_transmission(self, wavelengths):
-        # TODO -- error checking/handling, handle exterior grid points & out of bound wavelengths...
+
+        # Check for and remove out-of-bounds wavelengths to avoid RegularGridInterpolator throwing errors
+        wavelengths_trim = wavelengths[(self.wavelength_index[0] <= wavelengths) & (wavelengths <= self.wavelength_index[-1])]
+        if len(wavelengths_trim) != len(wavelengths):
+            print('WARNING: In atmosphere.get_transmission() -- some or all provided wavelengths are outside the current bounds of [' +
+            str(self.wavelength_index[0])+', '+str(self.wavelength_index[-1])+'], discarding invalid values')
+
+        # Perform trilinear interpolation to find transmission values
         interpolation = RegularGridInterpolator( (self.airmass_index, self.water_vapor_index, self.wavelength_index), self.transmission )
-        try:
-            return interpolation([[self.airmass.value, self.water_vapor.value, λ] for λ in wavelengths.to(u.angstrom).value]) * u.Unit('')
-        except ValueError:
-            print('ERROR in atmosphere.get_transmission(): requested wavelength out of bounds')
-            return []
+        return interpolation([[self.airmass.value, self.water_vapor.value, λ] for λ in wavelengths_trim.to(u.angstrom).value]) * u.Unit('')
 
 
     def get_emission(self, wavelengths):
-        # Check for airmass, water vapor values on grid points
-        #if self.airmass in self.airmass_index:
-        #    return []  # TODO
-        #if self.water_vapor in self.water_vapor_index:
-        #    return []  # TODO
+        # Check for and remove out-of-bounds wavelengths to avoid RegularGridInterpolator throwing errors
+        wavelengths_trim = wavelengths[(self.wavelength_index[0] <= wavelengths) & (wavelengths <= self.wavelength_index[-1])]
+        if len(wavelengths_trim) != len(wavelengths):
+            print('WARNING: In atmosphere.get_emission() -- some or all provided wavelengths are outside the current bounds of [' +
+            str(self.wavelength_index[0])+', '+str(self.wavelength_index[-1])+'], discarding invalid values')
 
-        # Get airmass & vapor index values closest to current airmass & vapor
-        emission = self._trilinear_interpolation(self.emission, wavelengths) * u.Unit('photon/(s arcsec^2 nm m^2)')
-
-        return emission
+        # Perform trilinear interpolation to find emission values
+        interpolation = RegularGridInterpolator( (self.airmass_index, self.water_vapor_index, self.wavelength_index), self.emission )
+        return interpolation([[self.airmass.value, self.water_vapor.value, λ] for λ in wavelengths_trim.to(u.angstrom).value]) * u.Unit('photon/(s arcsec^2 nm m^2)')
--- run_test.py
+++ run_test.py
@@ -8,30 +8,17 @@ from numpy.random import random
 
 if __name__ == '__main__':
     foo = instrument('nires')
-    #print(foo.get_throughput(u.Quantity([5000, 8000, 10000, 11000], u.angstrom)))
-    #print(foo.get_dark_current())
-    #print(foo.get_read_noise())
+    foo.get_throughput(u.Quantity([5000, 8000, 10000, 11000], u.angstrom))
+    foo.get_dark_current()
+    foo.get_read_noise()
 
 
     bar = atmosphere()
-    #bar.airmass=u.Quantity(1.3)
-    #bar.water_vapor=u.Quantity('4 mm')
-    print(bar.get_transmission(u.Quantity(random(10000)*4+1, u.micron)))
-    
-#     SETUP_CODE = '''
-# from Atmosphere import atmosphere
-# from numpy.random import random
-# import astropy.units as u
-# bar = atmosphere()
-# bar.airmass=u.Quantity(1.3)
-# bar.water_vapor=u.Quantity('4 mm')'''
-
-
-#     em_test = timeit.timeit(setup=SETUP_CODE, stmt='bar.get_emission(u.Quantity(random(1000)*4+1, u.micron))', number=10000)
-#     tr_test = timeit.timeit(setup=SETUP_CODE, stmt='bar.get_transmission(u.Quantity(random(1000)*4+1, u.micron))', number=10000)
-#     print('EMISSION TIME:',em_test)
-#     print('TRANSMISSION TIME:',tr_test)
+    bar.airmass=u.Quantity(1.3)
+    bar.water_vapor=u.Quantity('4.2 mm')
+    bar.get_transmission(u.Quantity(random(1000)*4, u.micron))
+    bar.get_emission(u.Quantity(random(1000)*4, u.micron))
 
     yum = source()
     yum.redshift = 2
-    #print(yum.get_flux(u.Quantity([0.3, 0.5, 0.8, 1, 1.5, 2, 2.5, 3], u.micron)))
+    yum.get_flux(u.Quantity([0.3, 0.5, 0.8, 1, 1.5, 2, 2.5, 3], u.micron))
